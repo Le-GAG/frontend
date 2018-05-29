23fd3520f3698278c9eb30e4009779fc1c4709e9
nstCactus <nstCactus@gmail.com>
2018-05-29 02:34:11 +0200

    🍱 Exclude moment locales (except fr & en) from bundle

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+const webpack = require('webpack');
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|fr/),
+    ],
+  },
+
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: false,
