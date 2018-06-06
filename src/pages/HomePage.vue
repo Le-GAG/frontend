2ac64e7b55e885268909d1731389b3171aa07aa5
nstCactus <nstCactus@gmail.com>
2018-06-06 22:43:20 +0200

    💄 Improve home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="home-page">
-    <router-link to="producteurs">Producteurs</router-link>
+  <div class="home-page section">
+    <div class="container">
+      <nav class="level">
+        <p class="level-item has-text-centered">
+          <router-link :to="{ name: 'producers' }">Producteurs</router-link>
+        </p>
+        <p class="level-item has-text-centered">
+          <router-link :to="{ name: 'products' }">Produits</router-link>
+        </p>
+        <p class="level-item has-text-centered">
+          <router-link :to="{ name: 'about' }">À propos</router-link>
+        </p>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    name: 'IndexPage',
+  };
 </script>
 
 <style scoped lang="scss">
