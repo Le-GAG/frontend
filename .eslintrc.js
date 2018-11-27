60f50f9910bffe05892087c950c17f264f6105a9
nstCactus <nstCactus@gmail.com>
2018-11-27 18:12:23 +0100

    🚨 Switch to prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   'extends':       [
     'eslint:recommended',
     'plugin:vue/recommended',
-    // '@vue/standard',
     '@vue/typescript',
+    'plugin:prettier/recommended',
   ],
   'plugins':       [
     'html',
